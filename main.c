714abc70ce0a62b4110c76a75bcefdc2a941a70e
LiloMarino <lilo.marino@hotmail.com>
2023-05-31 14:31:14 -0300

    test: Ajustado main para os casos de teste

--- main.c
+++ main.c
@@ -1,42 +1,77 @@
 #include "radialtree.h"
+#include "geo.h"
+#include "qry.h"
+#include "svg.h"
+#include "Bibliotecas/geradores.h"
 #include <string.h>
 #include <stdio.h>
+
 int main()
 {
-    RadialTree t = newRadialTree(8, 0.1);
-    insertRadialT(t, 0, 0, strdup("Ponto A"));
-    insertRadialT(t, 1, 0.5, strdup("Ponto C"));
-    insertRadialT(t, 0.5, 1, strdup("Ponto D"));
-    insertRadialT(t, -0.5, 1, strdup("Ponto E"));
-    insertRadialT(t, -1, 0.5, strdup("Ponto F"));
-    insertRadialT(t, -1, -0.5, strdup("Ponto G"));
-    insertRadialT(t, -0.5, -1, strdup("Ponto H"));
-    insertRadialT(t, 0.5, -1, strdup("Ponto I"));
-    insertRadialT(t, 1, -0.5, strdup("Ponto J"));
-    insertRadialT(t, 0.6, 0.2, strdup("Ponto K"));
-    Lista Teste;
-    // Imprime
-    printf("Area Total:\n");
-    Teste = createLst(-1);
-    getNodesDentroRegiaoRadialT(t, 1, -1, -1, 1, Teste);
-    while (!isEmptyLst(Teste))
-    {
-        Node n = popLst(Teste);
-        printf("%s\n", (char *)getInfoRadialT(t, n));
-    }
-    killLst(Teste);
-    // Remove os nós
-    removeNoRadialT(&t, getNodeRadialT(t, 1, -0.5, 0.1)); // Ponto K
-    removeNoRadialT(&t, getNodeRadialT(t, 1, 0.5, 0.1)); // Ponto C
-    // Imprime
-    printf("Area Total:\n");
-    Teste = createLst(-1);
-    getNodesDentroRegiaoRadialT(t, 1, -1, -1, 1, Teste);
-    while (!isEmptyLst(Teste))
-    {
-        Node n = popLst(Teste);
-        printf("%s\n", (char *)getInfoRadialT(t, n));
-    }
-    killLst(Teste);
-    freeRadialTree(&t, true);
+    FILE* log;
+
+    RadialTree All = newRadialTree(8, 0.1);
+    log = CriaLog("Registro","txt");
+    ArqGeo Geo00 = abreLeituraGeo("../00-caso-de-teste.geo");
+    ArqQry Qry00 = abreLeituraQry("../00-caso-de-teste.qry");
+    InterpretaGeo(Geo00,All);
+    OperaSVG("00-caso-de-teste.geo",All);
+    InterpretaQry(Qry00,All,log,"../logs/");
+    OperaSVG("00-caso-de-teste.qry",All);
+    fechaGeo(Geo00);
+    fechaQry(Qry00);
+    fclose(log);
+    freeRadialTree(&All, true);
+
+    All = newRadialTree(8, 0.1);
+    log = CriaLog("Registro","txt");
+    ArqGeo Geo01 = abreLeituraGeo("../01-caso-de-teste.geo");
+    ArqQry Qry01 = abreLeituraQry("../01-caso-de-teste.qry");
+    InterpretaGeo(Geo01,All);
+    OperaSVG("01-caso-de-teste.geo",All);
+    InterpretaQry(Qry01,All,log,"../logs/");
+    OperaSVG("01-caso-de-teste.qry",All);
+    fechaGeo(Geo01);
+    fechaQry(Qry01);
+    fclose(log);
+    freeRadialTree(&All, true);
+
+    All = newRadialTree(8, 0.1);
+    log = CriaLog("Registro","txt");
+    ArqGeo Geo02 = abreLeituraGeo("../02-caso-de-teste.geo");
+    ArqQry Qry02 = abreLeituraQry("../02-caso-de-teste.qry");
+    InterpretaGeo(Geo02,All);
+    OperaSVG("02-caso-de-teste.geo",All);
+    InterpretaQry(Qry02,All,log,"../logs/");
+    OperaSVG("02-caso-de-teste.qry",All);
+    fechaGeo(Geo02);
+    fechaQry(Qry02);
+    fclose(log);
+    freeRadialTree(&All, true);
+
+    All = newRadialTree(8, 0.1);
+    log = CriaLog("Registro","txt");
+    ArqGeo Geo03 = abreLeituraGeo("../03-caso-de-teste.geo");
+    ArqQry Qry03 = abreLeituraQry("../03-caso-de-teste.qry");
+    InterpretaGeo(Geo03,All);
+    OperaSVG("03-caso-de-teste.geo",All);
+    InterpretaQry(Qry03,All,log,"../logs/");
+    OperaSVG("03-caso-de-teste.qry",All);
+    fechaGeo(Geo03);
+    fechaQry(Qry03);
+    fclose(log);
+    freeRadialTree(&All, true);
+
+    All = newRadialTree(8, 0.1);
+    log = CriaLog("Registro","txt");
+    ArqGeo Geo04 = abreLeituraGeo("../04-caso-de-teste.geo");
+    ArqQry Qry04 = abreLeituraQry("../04-caso-de-teste.qry");
+    InterpretaGeo(Geo04,All);
+    OperaSVG("04-caso-de-teste.geo",All);
+    InterpretaQry(Qry04,All,log,"../logs/");
+    OperaSVG("04-caso-de-teste.qry",All);
+    fechaGeo(Geo04);
+    fechaQry(Qry04);
+    fclose(log);
+    freeRadialTree(&All, true);
 }
