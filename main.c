348496dca4fc2120edbe88e88629cc22336df211
LiloMarino <lilo.marino@hotmail.com>
2023-05-31 18:43:40 -0300

    test: Atualizado base.svg para os testes e removido os testes feitos pelo ChatGPT

--- main.c
+++ main.c
@@ -5,77 +5,10 @@
 #include "Bibliotecas/geradores.h"
 #include <string.h>
 #include <stdio.h>
-#define PERSONALIZADO 1 /* 0 = False, 1 = True */
 
 int main()
 {
     FILE* log;
-#if PERSONALIZADO == 0
-    RadialTree All = newRadialTree(8, 0.1);
-    log = CriaLog("../logs/Registro","txt");
-    ArqGeo Geo00 = abreLeituraGeo("../00-caso-de-teste.geo");
-    ArqQry Qry00 = abreLeituraQry("../00-caso-de-teste.qry");
-    InterpretaGeo(Geo00,All);
-    OperaSVG("00-caso-de-teste.geo",All);
-    InterpretaQry(Qry00,All,log,"../logs/");
-    OperaSVG("00-caso-de-teste.qry",All);
-    fechaGeo(Geo00);
-    fechaQry(Qry00);
-    fclose(log);
-    freeRadialTree(&All, true);
-
-    All = newRadialTree(8, 0.1);
-    log = CriaLog("../logs/Registro","txt");
-    ArqGeo Geo01 = abreLeituraGeo("../01-caso-de-teste.geo");
-    ArqQry Qry01 = abreLeituraQry("../01-caso-de-teste.qry");
-    InterpretaGeo(Geo01,All);
-    OperaSVG("01-caso-de-teste.geo",All);
-    InterpretaQry(Qry01,All,log,"../logs/");
-    OperaSVG("01-caso-de-teste.qry",All);
-    fechaGeo(Geo01);
-    fechaQry(Qry01);
-    fclose(log);
-    freeRadialTree(&All, true);
-
-    All = newRadialTree(8, 0.1);
-    log = CriaLog("../logs/Registro","txt");
-    ArqGeo Geo02 = abreLeituraGeo("../02-caso-de-teste.geo");
-    ArqQry Qry02 = abreLeituraQry("../02-caso-de-teste.qry");
-    InterpretaGeo(Geo02,All);
-    OperaSVG("02-caso-de-teste.geo",All);
-    InterpretaQry(Qry02,All,log,"../logs/");
-    OperaSVG("02-caso-de-teste.qry",All);
-    fechaGeo(Geo02);
-    fechaQry(Qry02);
-    fclose(log);
-    freeRadialTree(&All, true);
-
-    All = newRadialTree(8, 0.1);
-    log = CriaLog("../logs/Registro","txt");
-    ArqGeo Geo03 = abreLeituraGeo("../03-caso-de-teste.geo");
-    ArqQry Qry03 = abreLeituraQry("../03-caso-de-teste.qry");
-    InterpretaGeo(Geo03,All);
-    OperaSVG("03-caso-de-teste.geo",All);
-    InterpretaQry(Qry03,All,log,"../logs/");
-    OperaSVG("03-caso-de-teste.qry",All);
-    fechaGeo(Geo03);
-    fechaQry(Qry03);
-    fclose(log);
-    freeRadialTree(&All, true);
-
-    All = newRadialTree(8, 0.1);
-    log = CriaLog("../logs/Registro","txt");
-    ArqGeo Geo04 = abreLeituraGeo("../04-caso-de-teste.geo");
-    ArqQry Qry04 = abreLeituraQry("../04-caso-de-teste.qry");
-    InterpretaGeo(Geo04,All);
-    OperaSVG("04-caso-de-teste.geo",All);
-    InterpretaQry(Qry04,All,log,"../logs/");
-    OperaSVG("04-caso-de-teste.qry",All);
-    fechaGeo(Geo04);
-    fechaQry(Qry04);
-    fclose(log);
-    freeRadialTree(&All, true);
-#elif PERSONALIZADO == 1
     RadialTree All = newRadialTree(8, 0.1);
     log = CriaLog("../logs/Registro","txt");
     ArqGeo Geo00 = abreLeituraGeo("../caso-de-teste.geo");
@@ -88,5 +21,5 @@ int main()
     fechaQry(Qry00);
     fclose(log);
     freeRadialTree(&All, true);
-#endif
+
 }
