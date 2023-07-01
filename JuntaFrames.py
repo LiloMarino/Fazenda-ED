38e582ebff9ca0224e7867f67b0b7251fae1a8d6
LiloMarino <lilo.marino@hotmail.com>
2023-07-01 19:12:39 -0300

    Atualizado geração dos frames e adicionado juntor de frames

---
+++ JuntaFrames.py
@@ -0,0 +1,34 @@
+import glob
+import os
+
+def generate_html(svg_directory):
+    # Verifica se o diretório especificado existe
+    if not os.path.isdir(svg_directory):
+        print(f"O diretório '{svg_directory}' não existe.")
+        return
+    
+    # Obtém todos os arquivos .svg na pasta especificada e os ordena alfabeticamente
+    svgs = sorted(glob.glob(os.path.join(svg_directory, "*.svg")), key=os.path.basename)
+    
+    # Inicializa a string HTML
+    html = '<!DOCTYPE html">\n<HTML>\n<HEAD>\n<style>\n  img { display: block;  margin-left: 5px;  margin-right: auto;  max-width: 100%;} \n @media print {img {page-break-after: always;}} \n </style></HEAD>\n<BODY>\n'
+    
+    # Itera sobre os arquivos .svg e adiciona as tags correspondentes ao HTML
+    for i, svg_file in enumerate(svgs, start=1):
+        svg_filename = os.path.basename(svg_file)
+        html += f'<H1>[{i}] {svg_filename}</H1>\n'
+        html += f'<IMG SRC="{svg_filename}" NAME="{svg_filename}" />\n'
+    
+    # Fecha as tags HTML
+    html += '</BODY>\n</HTML>'
+    
+    # Define o caminho completo para o arquivo HTML na pasta atual
+    html_file_path = os.path.join(svg_directory, "output.html")
+    
+    # Salva o conteúdo HTML no arquivo
+    with open(html_file_path, "w") as file:
+        file.write(html)
+
+# Chama a função para gerar o HTML
+svg_directory = input("Informe o caminho da pasta que contém os arquivos .svg: ")
+generate_html(svg_directory)
--- src/svg.c
+++ src/svg.c
@@ -88,7 +88,7 @@ void CriaFrame(RadialTree All,char *OutputGeoQry, int *num)
     sprintf(nomepasta, "frames-%s", file);
     CriaPasta(path,nomepasta);
     char nome[strlen(path) + strlen(nomepasta) + strlen(file) + 10];
-    sprintf(nome, "%s/%s/%s-frame0%d", path, nomepasta, file, *num);
+    sprintf(nome, "%s/%s/%s-frame-%d", path, nomepasta, file, *num);
     (*num)++;
     OperaSVG(nome, All);
     free(path);
